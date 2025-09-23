b1709a762c8346c42e8c8dd382647dff95f73849
WALID FATH <walidfath02@gmail.com>
2025-09-23 17:31:09 +0100

    grafana comit

--- app/server.js
+++ app/server.js
@@ -107,4 +107,4 @@ if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
 }
 
-module.exports = app; // export pour Supertest
+module.exports = app;
